658cbdd14b1be561ebc0dbce9066478e3833c43a
datasave582-design <datasave582@gmail.com>
2026-08-09 17:08:32 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,25 @@
-/* =========================================================
-   CSC SUITE - FIREBASE CONFIGURATION
-   ========================================================= */
+/* ============================================================
+   CSC SUITE - FIREBASE CONFIG
+   File: firebase-config.js
+   ============================================================ */
 
 window.CSC_FIREBASE_CONFIG = {
     apiKey: "AIzaSyA5LBLlGP83ol2nqWFrPmDS3zYMyA5c28",
-
     authDomain: "cricket-c3052.firebaseapp.com",
-
     databaseURL: "https://cricket-c3052-default-rtdb.firebaseio.com",
-
     projectId: "cricket-c3052",
-
     storageBucket: "cricket-c3052.firebasestorage.app",
-
     messagingSenderId: "980803711949",
-
     appId: "1:980803711949:web:8151d0a9903d8de5afca84",
-
     measurementId: "G-M92NCSR69W"
 };
+
+/*
+=============================================================
+IMPORTANT
+
+This file contains the Firebase WEB configuration.
+
+Do NOT put Firebase Admin SDK credentials here.
+=============================================================
+*/
