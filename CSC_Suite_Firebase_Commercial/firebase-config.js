54fd998cb3cf4ee2196bf86748d97d4d31c3359f
datasave582-design <datasave582@gmail.com>
2026-08-09 16:27:26 +0530

    Add files via upload

---
+++ CSC_Suite_Firebase_Commercial/firebase-config.js
@@ -0,0 +1,18 @@
+/*
+ * CSC Suite - Central Firebase Configuration
+ * 
+ * IMPORTANT:
+ * Replace ONLY the values marked PASTE_FROM_FIREBASE_CONSOLE.
+ * Get the exact Web App config from:
+ * Firebase Console -> Project Settings -> Your apps -> Web app
+ */
+window.CSC_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyA5LBLlGP83ol2nQwFrPmfDS3zYMyA5c28",
+  authDomain: "cricket-c3052.firebaseapp.com",
+  databaseURL: "https://cricket-c3052-default-rtdb.firebaseio.com",
+  projectId: "cricket-c3052",
+  storageBucket: "cricket-c3052.firebasestorage.app",
+  messagingSenderId: "980803711949",
+  appId: "1:980803711949:web:8151d0a9903d8de5afca84",
+  measurementId: "G-M92NCSR69W"
+};
